890140d0234ca3cfb2b9902654270fc0e683216b
Ajneb Al Revés <80727117+AjnebAlReves@users.noreply.github.com>
2024-12-06 13:04:16 +0000

    Arreglitos

--- src/components/infopages/HostingCard.jsx
+++ src/components/infopages/HostingCard.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 const HostingCard = ({ hosting }) => {
     return (
+        <>
         <div className="bg-gray-100 rounded-xl p-6 shadow-lg hover:shadow-2xl transition-shadow duration-300 w-full max-w-xs">
             <img src={hosting.logo} alt={`${hosting.name} logo`} className="w-16 h-16 mb-4" />
             <h2 className="text-xl font-bold">{hosting.name}</h2>
@@ -27,6 +28,19 @@ const HostingCard = ({ hosting }) => {
                 Más Información
             </a>
         </div>
+        <div className="bg-gray-100 rounded-xl p-6 shadow-lg hover:shadow-2xl transition-shadow duration-300 w-full max-w-xs">
+            <img src='https://example.com' alt={`Añadir servidor logo`} className="w-16 h-16 mb-4" />
+            <p className="text-gray-700 mt-2 mb-4">¿Quieres añadir tu hosting? ¡Pues, añádelo! Envía un Issue para que te añadamos aquí</p>
+            <a
+                href="https://github.com/MultiPtero/dashboard/issues"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-green-500 text-white px-4 py-2 rounded-lg font-semibold hover:bg-green-600 transition-colors duration-300 block text-center"
+            >
+                Más Información
+            </a>
+        </div>
+        </>
     );
 };
 
